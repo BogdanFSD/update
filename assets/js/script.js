21daf19fd27a43052a2bc8e4949362eebddcfa53
Bogdandev <luitko007@gmail.com>
2023-03-13 18:11:43 +0000

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 // start button
 const mainContainer = document.querySelector('.container');
 const start = document.querySelector(".start-btn");
@@ -56,7 +57,7 @@ function startGame (){
   end.classList.add('hide')
   questionContainer.classList.remove('hide');
   gamerules.classList.add('hide');
-  mixedQuestion = questions.sort(() => Math.random() - 0.5).slice(0, 2);
+  mixedQuestion = questions.sort(() => Math.random() - 0.5).slice(0, 5);
   questionIndex = 0;
   score = 0;
   
@@ -139,7 +140,7 @@ function endQuiz() {
   const elapsedTime = (quizEndTime - quizStartTime) / 1000 - 2;
 
   questionContainer.classList.add('hide');
-  document.getElementById("presult").innerHTML = `Your Score: ${score} / ${mixedQuestion.length}<br>You spent: ${elapsedTime.toFixed(1)} seconds to solve this quiz.<br> Can you make better?`;
+  document.getElementById("presult").innerHTML = `Your Score: ${score} / ${mixedQuestion.length}<br>You spent: ${elapsedTime.toFixed(1)} seconds to solve this quiz.<br> Can you do better?`;
   endContainer.classList.remove('hide');
 };
 
@@ -158,104 +159,194 @@ function startTimer() {
 }
 
 
-
-
-
-
-
-
-
-
-
 const questions = [
-    {
-      question: 'Who is the evil in Harry Potter?',
+  {
+    question: 'Who is the evil in Harry Potter?',
+    answers: [
+      { text: 'Voldemort', correct: true },
+      { text: 'Severus Snape', correct: false },
+      { text: 'Dobby', correct: false },
+      { text: 'Gandalf', correct: false }
+    ]
+  },
+  {
+    question: 'What happened with Don Quixote?',
+    answers: [
+      { text: 'Miguel de Cervantes', correct: true },
+      { text: 'Mark Twain ', correct: false },
+      { text: 'Cillian Murphy', correct: false },
+      { text: 'Sancho Panza', correct: false }
+    ]
+  },
+  {
+    question: 'What character is Queequeg ?',
+    answers: [
+      { text: 'Captain', correct: false },
+      { text: 'Cannibal ', correct: true },
+      { text: 'Moby Dick', correct: false },
+      { text: 'Pirat', correct: false }
+    ]
+  },
+  {
+    question: 'Because of what Hamlet died?',
+    answers: [
+      { text: 'Car accident', correct: false },
+      { text: 'Poison', correct: false },
+      { text: 'Rapier', correct: false },
+      { text: 'Poisoned rapier', correct: true }
+    ]
+  },
+  {
+      question: 'What is the second Harry Potter book called?',
       answers: [
-        { text: 'Voldemort', correct: true },
-        { text: 'Severus Snape', correct: false },
-        { text: 'Dobby', correct: false },
-        { text: 'Gandalf', correct: false }
+        { text: 'Jane Eyre', correct: false },
+        { text: 'Oliver Twist', correct: false },
+        { text: 'Dragon', correct: false },
+        { text: 'Harry Potter & The Chamber of Secrets', correct: true }
       ]
     },
     {
-      question: 'What happened with Don Quixote?',
+      question: 'Who wrote Frankenstein?',
       answers: [
-        { text: 'Miguel de Cervantes', correct: true },
-        { text: 'Mark Twain ', correct: false },
+        { text: 'Mary Shelley', correct: true },
+        { text: 'Danielle Steel', correct: false },
+        { text: 'J. K. Rowling', correct: false },
+        { text: 'Osamu Tezuka', correct: false }
+      ]
+    },
+    {
+      question: 'What wrote Homage to Catalonia?',
+      answers: [
+        { text: 'George Orwell', correct: true },
+        { text: 'Irving Wallace ', correct: false },
         { text: 'Cillian Murphy', correct: false },
         { text: 'Sancho Panza', correct: false }
       ]
     },
     {
-      question: 'What character is Queequeg ?',
+      question: 'Who took pen name as Eric Arthur Blair?',
       answers: [
-        { text: 'Captain', correct: false },
-        { text: 'Cannibal ', correct: true },
-        { text: 'Moby Dick', correct: false },
-        { text: 'Pirat', correct: false }
+        { text: 'Mark Twain', correct: false },
+        { text: 'George Orwell ', correct: true },
+        { text: 'Stan Lee', correct: false },
+        { text: 'Dr. Seuss', correct: false }
       ]
     },
     {
-      question: 'Because of what Hamlet died?',
+      question: 'Name the first book in the Noughts and Crosses series?',
       answers: [
-        { text: 'Car accident', correct: false },
-        { text: 'Poison', correct: false },
-        { text: 'Rapier', correct: false },
-        { text: 'Poisoned rapier', correct: true }
+        { text: 'Noughts & Crosses', correct: false },
+        { text: 'Knife Edge', correct: false },
+        { text: 'Double Cross', correct: false },
+        { text: 'Black & White', correct: true }
       ]
     },
     {
-        question: 'What is the second Harry Potter book called?',
+        question: 'What was the name of Charles Dickens last book which was left unfinished?',
         answers: [
-          { text: 'Jane Eyre', correct: false },
+          { text: 'The Old Man of Lochnagar', correct: false },
           { text: 'Oliver Twist', correct: false },
-          { text: 'Dragon', correct: false },
-          { text: 'Harry Potter & The Chamber of Secrets', correct: true }
+          { text: 'Charlotte, Emily and Anne Bronte', correct: false },
+          { text: 'The Mystery Of Edwin Drood', correct: true }
         ]
-      },
-      {
-        question: 'Who wrote Frankenstein?',
-        answers: [
-          { text: 'Mary Shelley', correct: true },
-          { text: 'Danielle Steel', correct: false },
-          { text: 'J. K. Rowling', correct: false },
-          { text: 'Osamu Tezuka', correct: false }
-        ]
-      },
-      {
-        question: 'What wrote Homage to Catalonia?',
-        answers: [
-          { text: 'George Orwell', correct: true },
-          { text: 'Irving Wallace ', correct: false },
-          { text: 'Cillian Murphy', correct: false },
-          { text: 'Sancho Panza', correct: false }
-        ]
-      },
-      {
-        question: 'Who took pen name as Eric Arthur Blair?',
-        answers: [
-          { text: 'Mark Twain', correct: false },
-          { text: 'George Orwell ', correct: true },
-          { text: 'Stan Lee', correct: false },
-          { text: 'Dr. Seuss', correct: false }
-        ]
-      },
-      {
-        question: 'Name the first book in the Noughts and Crosses series?',
-        answers: [
-          { text: 'Noughts & Crosses', correct: false },
-          { text: 'Knife Edge', correct: false },
-          { text: 'Double Cross', correct: false },
-          { text: 'Black & White', correct: true }
-        ]
-      },
-      {
-          question: 'What was the name of Charles Dickens last book which was left unfinished?',
-          answers: [
-            { text: 'The Old Man of Lochnagar', correct: false },
-            { text: 'Oliver Twist', correct: false },
-            { text: 'Charlotte, Emily and Anne Bronte', correct: false },
-            { text: 'The Mystery Of Edwin Drood', correct: true }
-          ]
-        }
-  ];
+    },
+    {
+      question: 'Which famous writer said, "A woman is only a woman, but a good cigar is a smoke"?',
+      answers: [
+        { text: 'Agatha Christie', correct: false },
+        { text: 'Charles Dickens', correct: false },
+        { text: 'William Shakespeare', correct: false },
+        { text: 'Rudyard Kipling', correct: true }
+      ]
+    },
+    {
+      question: 'Joanne Harris wrote which book, now made into a hit film starring Johnny Depp and Juliette Binoche?',
+      answers: [
+        { text: 'Blackberry Wine ', correct: false },
+        { text: 'Chocolat', correct: true },
+        { text: 'Gentlemen & Players ', correct: false },
+        { text: 'The Lollipop Shoes', correct: false }
+      ]
+    },
+    {
+      question: 'Which travel writer comes from De Moines, Iwoa?',
+      answers: [
+        { text: 'Robert Ludlum ', correct: false },
+        { text: 'Bill Bryson', correct: true },
+        { text: ' Enid Blyton ', correct: false },
+        { text: 'Alistair Maclean', correct: false }
+      ]
+    },
+    {
+      question: 'Who created Miss Marple?',
+      answers: [
+        { text: 'Rosemary Conley ', correct: false },
+        { text: 'Bill Bryson', correct: false },
+        { text: ' Douglas Adams ', correct: false },
+        { text: 'Agatha Christie', correct: true }
+      ]
+    },
+    {
+      question: 'What was the first book in English to be printed in England?',
+      answers: [
+        { text: 'Crime and Punishement ', correct: false },
+        { text: 'The Canterbury tales', correct: true },
+        { text: ' Harry Potter ', correct: false },
+        { text: 'Mary Poppins', correct: false }
+      ]
+    },
+    {
+      question: 'Who was responsible for the Complete Hip and Thigh Diet?',
+      answers: [
+        { text: 'The Scarlet Pimpernel ', correct: false },
+        { text: 'Macbeth', correct: false },
+        { text: ' Rosemary Conley ', correct: true },
+        { text: ' John Bunyan', correct: false }
+      ]
+    },
+    {
+      question: 'What type of book is the OED?',
+      answers: [
+        { text: 'Dictionary ', correct: true },
+        { text: 'List', correct: false },
+        { text: ' Kindle ', correct: false },
+        { text: 'Tuple', correct: false }
+      ]
+    },
+    {
+      question: 'According to Sue Townsend how old was Adrian Mole when he first started writing his diary?',
+      answers: [
+        { text: '15', correct: false },
+        { text: '13', correct: true },
+        { text: '43', correct: false },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Crockfords is a reference book relating to which group of people?',
+      answers: [
+        { text: 'Punks ', correct: false },
+        { text: 'Clergy of the church of England', correct: true },
+        { text: 'Football funs', correct: false },
+        { text: 'Prisoners', correct: false }
+      ]
+    },
+    {
+      question: 'If you were reading a book published by Fodor, what would the subject be?',
+      answers: [
+        { text: 'Travel ', correct: true },
+        { text: 'Painting', correct: false },
+        { text: 'War', correct: false },
+        { text: 'Friendship', correct: false }
+      ]
+    }
+];
+
+
+
+
+
+
+
+
+
